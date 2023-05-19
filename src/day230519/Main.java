8a45bc26bdde54a619bc676300b0512ac3e4a705
SEOHONU <shw12050708@gmail.com>
2023-05-19 23:24:23 +0900

    230519

---
+++ src/day230519/Main.java
@@ -0,0 +1,18 @@
+package day230519;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int y = Integer.parseInt(br.readLine());
+		bw.write(y - 543 + "\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
