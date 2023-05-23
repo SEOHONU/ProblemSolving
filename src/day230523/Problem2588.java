3367b1a0a005212fb4c50999bbd27949d1bdd2b1
SEOHONU <shw12050708@gmail.com>
2023-05-23 18:58:36 +0900

    230523

--- src/day230523/Problem2588.java
+++ src/day230523/Problem2588.java
@@ -5,7 +5,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class Problem2588 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
