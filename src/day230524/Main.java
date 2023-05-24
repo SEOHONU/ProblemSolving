e1994b2434b76576b5090be69a3ec9621c637e1f
SEOHONU <shw12050708@gmail.com>
2023-05-24 23:12:00 +0900

    230524

---
+++ src/day230524/Main.java
@@ -0,0 +1,19 @@
+package day230524;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		br.close();
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long C = Long.parseLong(st.nextToken());
+		bw.write(A + B + C + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
---
+++ src/day230524/Problem11382.java
@@ -0,0 +1,22 @@
+package day230524;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem11382 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		br.close();
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long C = Long.parseLong(st.nextToken());
+		bw.write(A + B + C + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
