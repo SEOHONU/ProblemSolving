3e9f114088328ae99b02f56f9d1aaaca2145ab5a
SEOHONU <shw12050708@gmail.com>
2023-06-06 22:50:12 +0900

    230606

---
+++ src/day230606/Problem15552.java
@@ -0,0 +1,23 @@
+package day230606;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem15552 {
+	public static void main(String[] args) throws Exception {
+		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
+			long T = Long.parseLong(br.readLine());
+			for (int i = 0; i < T; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				int A = Integer.parseInt(st.nextToken());
+				int B = Integer.parseInt(st.nextToken());
+				bw.write(A + B + "\n");
+			}
+			bw.flush();
+		}
+	}
+}
