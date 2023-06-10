5df737ebe50441c7b02cd459d3bc287049edbc08
SEOHONU <shw12050708@gmail.com>
2023-06-10 18:52:18 +0900

    230610

---
+++ src/day230610/Main.java
@@ -0,0 +1,23 @@
+package day230610;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
+			String check;
+			while ((check = br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(check, " ");
+				int A = Integer.parseInt(st.nextToken());
+				int B = Integer.parseInt(st.nextToken());
+				bw.write(A + B + "\n");
+			}
+			bw.flush();
+		}
+	}
+}
---
+++ src/day230610/Problem10952.java
@@ -0,0 +1,27 @@
+package day230610;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem10952 {
+	public static void main(String[] args) throws Exception {
+		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
+			while (true) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				int A = Integer.parseInt(st.nextToken());
+				int B = Integer.parseInt(st.nextToken());
+				int C = A + B;
+				if (C == 0) {
+					break;
+				} else {
+					bw.write(C + "\n");
+				}
+			}
+			bw.flush();
+		}
+	}
+}
